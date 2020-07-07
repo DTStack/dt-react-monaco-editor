838351f4e5e5735a7a69d99bc96ab7e9d33f02de
jeromeZhang <jingyu@dtstack.com>
2020-07-07 19:19:10 +0800

    feat: add package and change title

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../src/stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,12 +3,12 @@ import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
 
 export default {
-  title: 'Welcome',
+  title: 'Editor',
   component: Welcome,
 };
 
 export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
 
 ToStorybook.story = {
-  name: 'to Storybook',
+  name: 'Editor',
 };
