67a08f4ef5f9abd5a72e3b481a2a261a210d756f
jeromeZhang <jingyu@dtstack.com>
2020-07-08 09:59:23 +0800

    feat: add editor code

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { linkTo } from '@storybook/addon-links';
-import { Welcome } from '@storybook/react/demo';
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
 
-export default {
-  title: 'Editor',
-  component: Welcome,
-};
-
-export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
-
-ToStorybook.story = {
-  name: 'Editor',
-};
+const stories = storiesOf('Editor 编辑器', module);
+stories.add('Editor', () => {
+    return (
+        <div className='story_wrapper'>
+            <h2>何时使用</h2>
+            <p>页面需使用编辑器时</p>
+            <h2>示例</h2>
+        </div>
+    )
+}, {
+    info: {
+        text: `
+        代码示例：
+        ~~~js
+        ~~~
+        `
+    }
+})
