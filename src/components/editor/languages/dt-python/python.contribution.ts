1f7a49c8676bca477f6ecb476404ce1d7a49caa1
jeromeZhang <jingyu@dtstack.com>
2020-10-21 16:01:33 +0800

    feat(python): added Python syntax support

---
+++ src/components/editor/languages/dt-python/python.contribution.ts
@@ -0,0 +1,21 @@
+/* ---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *-------------------------------------------------------------------------------------------- */
+
+import { registerLanguage } from 'monaco-editor/esm/vs/basic-languages/_.contribution';
+import { registeCompleteItemsProvider, disposeProvider, onChange } from './simpleComplete';
+
+declare var monaco: any;
+declare var self: any;
+
+// Allow for running under nodejs/requirejs in tests
+var _monaco = typeof monaco === 'undefined' ? self.monaco : monaco;
+registerLanguage({
+    id: 'dtpython',
+    extensions: ['.py', '.rpy', '.pyw', '.cpy', '.gyp', '.gypi'],
+    aliases: ['Python', 'py'],
+    firstLine: '^#!/.*\\bpython[0-9.-]*\\b',
+    loader: function () { return _monaco.Promise.wrap(import('./python')); }
+});
+export { registeCompleteItemsProvider, disposeProvider, onChange };
